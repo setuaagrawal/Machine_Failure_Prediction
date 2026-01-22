007b6bf658109395cfd59482bf5e53e3be131780
setuaagrawal <31098692+setuaagrawal@users.noreply.github.com>
2026-01-23 01:56:33 +0530

    Update hosting.py

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -3,8 +3,9 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="week_3_mls/deployment",     # the local folder containing your files
+    folder_path="deployment",     # the local folder containing your files
     repo_id="setuagrawal/Machine-Failure-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
+
